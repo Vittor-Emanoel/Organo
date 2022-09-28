8dce046092613b1232a63f6cdc04911b472c9b57
= <=>
2022-09-28 15:12:20 -0300

    Lista suspensa Criada

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,17 @@
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = () => {
+  const times = [
+    "Escola de Programação",
+    "Front-End",
+    "Devops",
+    "UX & Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
+
   return (
     <section className="formulario">
       <form>
@@ -9,7 +19,7 @@ const Formulario = () => {
         <CampoTexto label="Nome" placeholder="Digite seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite seu Cargo" />
         <CampoTexto label="Imagem" placeholder="Informe o endereço da Imagem" />
-        ;
+        <ListaSuspensa label="Time" itens={times} />
       </form>
     </section>
   );
