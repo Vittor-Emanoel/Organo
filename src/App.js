bafc7fc6e2a902283b42424218bf2e4c2f9e85fe
= <=>
2022-09-29 17:47:24 -0300

    Cadastrando colaboradores inicio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import Banner from "./componentes/Banner/index.js";
 import Formulario from "./componentes/Formulario";
+import Time from "./componentes/Time";
 
 function App() {
+  const [colaboradores, setColaboradores] = useState([]);
+
+  const aoNovoColaborarAdicionado = (colaborador) => {
+    console.log(colaborador);
+  };
+
   return (
     <>
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={aoNovoColaborarAdicionado} />
+      <Time />
     </>
   );
 }
