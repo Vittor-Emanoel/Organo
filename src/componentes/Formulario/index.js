6f5d0f9fe71103033e10dc601d6c879cf5e224e7
= <=>
2022-09-29 13:35:24 -0300

    Escutando o submit

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -13,18 +13,34 @@ const Formulario = () => {
     "Inovação e Gestão",
   ];
 
+  const aoSalvar = (e) => {
+    e.preventDefault();
+    console.log("Form foi submetido");
+  };
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu Cargo" />
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu Cargo"
+        />
         <CampoTexto label="Imagem" placeholder="Informe o endereço da Imagem" />
-        <ListaSuspensa label="Time" itens={times} />
-        <Botao texto="Criar card" />
+
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
 };
 
 export default Formulario;
+
+//SE QUISER PASSAR MAIS DE UMA PROP DENTRO DE UM COMPONENTE, PASSE O CHILDREN - LINHA 24
