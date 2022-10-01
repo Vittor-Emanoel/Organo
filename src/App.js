6d33b2876577a3e6dc2f88b2173c199d3fcb0757
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-10-01 12:26:48 -0300

    Curso Finalizado

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ function App() {
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaborarAdicionado = (colaborador) => {
+    debugger;
     setColaboradores([...colaboradores, colaborador]);
   };
 
