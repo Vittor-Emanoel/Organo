d2a577c8b73f1721afe26cdc9c3fe997e5e1c12b
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-10-01 01:01:51 -0300

    Limpando Inputs & escondendo os times vazios

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -4,23 +4,23 @@ import "./Time.css";
 const Time = (props) => {
   const css = { backgroundColor: props.corSecundario };
 
-  return (
-    props.colaboradores.length > 0 && (
-      <section className="time" style={css}>
-        <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-        <div className="colaboradores">
-          {props.colaboradores.map((colaborador) => (
-            <Colaborador
-              key={colaborador.nome}
-              nome={colaborador.nome}
-              cargo={colaborador.cargo}
-              imagem={colaborador.imagem}
-              corDeFundo={props.corPrimaria}
-            />
-          ))}
-        </div>
-      </section>
-    )
+  return props.colaboradores.length > 0 ? (
+    <section className="time" style={css}>
+      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+      <div className="colaboradores">
+        {props.colaboradores.map((colaborador) => (
+          <Colaborador
+            key={colaborador.nome}
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+            corDeFundo={props.corPrimaria}
+          />
+        ))}
+      </div>
+    </section>
+  ) : (
+    ""
   );
 };
 
